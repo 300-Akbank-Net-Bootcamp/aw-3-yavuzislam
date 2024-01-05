1900a5b55c5b34c1d847d75179d0a17c2aa8c1cb
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:11:19 +0300

    Update AddressValidator.cs

--- VbApi/Vb.Business/Validator/AddressValidator.cs
+++ VbApi/Vb.Business/Validator/AddressValidator.cs
@@ -7,8 +7,29 @@ public class CreateAddressValidator : AbstractValidator<AddressRequest>
 {
     public CreateAddressValidator()
     {
-        RuleFor(x => x.PostalCode).NotEmpty().MaximumLength(6).MinimumLength(6).WithName("Zip code or postal code");
-        RuleFor(x => x.Address1).NotEmpty().MinimumLength(20).MaximumLength(100).WithName("Customer address line 1");
-        RuleFor(x => x.Address2).NotEmpty().MaximumLength(100).WithName("Customer address line 2");
+        RuleFor(x => x.CustomerId)
+            .NotEmpty().WithMessage("CustomerId field is required.")
+            .GreaterThan(0).WithMessage("CustomerId field must be a valid value.");
+
+        RuleFor(x => x.Address1)
+            .NotEmpty().WithMessage("Address1 field is required.")
+            .MaximumLength(150).WithMessage("Address1 field must be at most 150 characters.");
+
+        RuleFor(x => x.Address2)
+            .MaximumLength(150).WithMessage("Address2 field must be at most 150 characters.");
+
+        RuleFor(x => x.Country)
+            .NotEmpty().WithMessage("Country field is required.")
+            .MaximumLength(100).WithMessage("Country field must be at most 100 characters.");
+
+        RuleFor(x => x.City)
+            .NotEmpty().WithMessage("City field is required.")
+            .MaximumLength(100).WithMessage("City field must be at most 100 characters.");
+
+        RuleFor(x => x.County)
+            .MaximumLength(100).WithMessage("County field must be at most 100 characters.");
+
+        RuleFor(x => x.PostalCode)
+            .MaximumLength(10).WithMessage("PostalCode field must be at most 10 characters.");
     }
 }
