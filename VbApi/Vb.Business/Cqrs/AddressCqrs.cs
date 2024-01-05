a78e5018eb5ad90398c54f73a2cc9d828f4391b0
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:53 +0300

    Update AddressCqrs.cs

--- VbApi/Vb.Business/Cqrs/AddressCqrs.cs
+++ VbApi/Vb.Business/Cqrs/AddressCqrs.cs
@@ -6,3 +6,9 @@ namespace Vb.Business.Cqrs;
 
 public record GetAllAddressQuery() : IRequest<ApiResponse<List<AddressResponse>>>;
 public record GetAddressByIdQuery(int Id) : IRequest<ApiResponse<AddressResponse>>;
+
+public record GetAddressByParameterQuery(int CustomerId, bool IsDefault) : IRequest<ApiResponse<List<AddressResponse>>>;
+
+public record CreateAddressCommand(AddressRequest Model) : IRequest<ApiResponse<AddressResponse>>;
+public record UpdateAddressCommand(int Id, AddressRequest Model) : IRequest<ApiResponse>;
+public record DeleteAddressCommand(int Id) : IRequest<ApiResponse>;
