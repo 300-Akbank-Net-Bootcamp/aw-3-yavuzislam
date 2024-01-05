cced80e7af6fc5665a7531061dcaa737a8d79e49
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:55 +0300

    Create ContactCqrs.cs

---
+++ VbApi/Vb.Business/Cqrs/ContactCqrs.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Vb.Base.Response;
+using Vb.Schema;
+
+namespace Vb.Business.Cqrs;
+
+public record GetAllContactQuery() : IRequest<ApiResponse<List<ContactResponse>>>;
+public record GetContactByIdQuery(int Id) : IRequest<ApiResponse<ContactResponse>>;
+public record GetContactByParameterQuery(string ContactType) : IRequest<ApiResponse<List<ContactResponse>>>;
+
+public record CreateContactCommand(ContactRequest Model) : IRequest<ApiResponse<ContactResponse>>;
+public record UpdateContactCommand(int Id, ContactRequest Model) : IRequest<ApiResponse>;
+public record DeleteContactCommand(int Id) : IRequest<ApiResponse>;
