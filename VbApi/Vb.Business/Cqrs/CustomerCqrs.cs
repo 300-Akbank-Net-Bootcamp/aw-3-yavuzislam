3b0dcc499e974fbad4e6f3ee651345930e416ef0
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:57 +0300

    Update CustomerCqrs.cs

--- VbApi/Vb.Business/Cqrs/CustomerCqrs.cs
+++ VbApi/Vb.Business/Cqrs/CustomerCqrs.cs
@@ -5,10 +5,10 @@ using Vb.Schema;
 namespace Vb.Business.Cqrs;
 
 
-public record CreateCustomerCommand(CustomerRequest Model) : IRequest<ApiResponse<CustomerResponse>>;
-public record UpdateCustomerCommand(int Id,CustomerRequest Model) : IRequest<ApiResponse>;
-public record DeleteCustomerCommand(int Id) : IRequest<ApiResponse>;
-
 public record GetAllCustomerQuery() : IRequest<ApiResponse<List<CustomerResponse>>>;
 public record GetCustomerByIdQuery(int Id) : IRequest<ApiResponse<CustomerResponse>>;
 public record GetCustomerByParameterQuery(string FirstName,string LastName,string IdentiyNumber) : IRequest<ApiResponse<List<CustomerResponse>>>;
+
+public record CreateCustomerCommand(CustomerRequest Model) : IRequest<ApiResponse<CustomerResponse>>;
+public record UpdateCustomerCommand(int Id,CustomerRequest Model) : IRequest<ApiResponse>;
+public record DeleteCustomerCommand(int Id) : IRequest<ApiResponse>;
