2ee28895a10e94937a2c9d30b9342abdcd78511f
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:49 +0300

    Create AccountTransactionCqrs.cs

---
+++ VbApi/Vb.Business/Cqrs/AccountTransactionCqrs.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Vb.Base.Response;
+using Vb.Schema;
+
+namespace Vb.Business.Cqrs;
+
+public record GetAllAccountTransactionQuery() : IRequest<ApiResponse<List<AccountTransactionResponse>>>;
+public record GetAccountTransactionByIdQuery(int Id) : IRequest<ApiResponse<AccountTransactionResponse>>;
+public record GetAccountTransactionByParameterQuery(string ReferenceNumber, string TransferType) : IRequest<ApiResponse<List<AccountTransactionResponse>>>;
+
+public record CreateAccountTransactionCommand(AccountTransactionRequest Model) : IRequest<ApiResponse<AccountTransactionResponse>>;
+public record UpdateAccountTransactionCommand(int Id, AccountTransactionRequest Model) : IRequest<ApiResponse>;
+public record DeleteAccountTransactionCommand(int Id) : IRequest<ApiResponse>;
