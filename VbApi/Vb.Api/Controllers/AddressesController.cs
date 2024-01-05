b75e673551e5ce01a371ce92d897c222c20b4a01
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:09:59 +0300

    Update AddressesController.cs

--- VbApi/Vb.Api/Controllers/AddressesController.cs
+++ VbApi/Vb.Api/Controllers/AddressesController.cs
@@ -31,5 +31,27 @@ public class AddressesController : ControllerBase
         var result = await mediator.Send(operation);
         return result;
     }
-    
+    [HttpPost]
+    public async Task<ApiResponse<AddressResponse>> Post([FromBody] AddressRequest address)
+    {
+        var operation = new CreateAddressCommand(address);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] AddressRequest address)
+    {
+        var operation = new UpdateAddressCommand(id,address);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteAddressCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
 }
