100f94d18fda2b6b3afba78a8c75644bb841dd8f
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:38 +0300

    Create AccountTransactionCommandHandler.cs

---
+++ VbApi/Vb.Business/Command/AccountTransactionCommandHandler.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Vb.Base.Response;
+using Vb.Business.Cqrs;
+using Vb.Data;
+using Vb.Data.Entity;
+using Vb.Schema;
+
+namespace Vb.Business.Command;
+
+public class AccountTransactionCommandHandler :
+    IRequestHandler<CreateAccountTransactionCommand, ApiResponse<AccountTransactionResponse>>,
+    IRequestHandler<UpdateAccountTransactionCommand, ApiResponse>,
+    IRequestHandler<DeleteAccountTransactionCommand, ApiResponse>
+
+{
+    private readonly VbDbContext dbContext;
+    private readonly IMapper mapper;
+
+    public AccountTransactionCommandHandler(VbDbContext dbContext, IMapper mapper)
+    {
+        this.dbContext = dbContext;
+        this.mapper = mapper;
+    }
+
+    public async Task<ApiResponse<AccountTransactionResponse>> Handle(CreateAccountTransactionCommand request,
+        CancellationToken cancellationToken)
+    {
+        var checkReferenceNumber = await dbContext.Set<AccountTransaction>()
+            .AnyAsync(x => x.ReferenceNumber == request.Model.ReferenceNumber, cancellationToken);
+        if (checkReferenceNumber)
+            return new ApiResponse<AccountTransactionResponse>("Reference number already exists");
+
+        var entity = mapper.Map<AccountTransactionRequest, AccountTransaction>(request.Model);
+        var entityResult = await dbContext.AddAsync(entity, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        var mapped = mapper.Map<AccountTransaction, AccountTransactionResponse>(entityResult.Entity);
+        return new ApiResponse<AccountTransactionResponse>(mapped);
+    }
+
+    public async Task<ApiResponse> Handle(UpdateAccountTransactionCommand request, CancellationToken cancellationToken)
+    {
+        var fromdb = await dbContext.Set<AccountTransaction>().Where(x => x.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+        if (fromdb == null)
+        {
+            return new ApiResponse("Record not found");
+        }
+
+        fromdb.TransferType = request.Model.TransferType;
+        fromdb.Amount = request.Model.Amount;
+        fromdb.Description = request.Model.Description;
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new ApiResponse();
+    }
+
+    public async Task<ApiResponse> Handle(DeleteAccountTransactionCommand request, CancellationToken cancellationToken)
+    {
+        var fromdb = await dbContext.Set<AccountTransaction>().Where(x => x.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (fromdb == null)
+        {
+            return new ApiResponse("Record not found");
+        }
+
+        fromdb.IsActive = false;
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return new ApiResponse();
+    }
+}
