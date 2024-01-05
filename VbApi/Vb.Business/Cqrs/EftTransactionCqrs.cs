6d2d60b34cf1cc6f5f37fa7c7d3d2953e7ef92f4
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:59 +0300

    Create EftTransactionCqrs.cs

---
+++ VbApi/Vb.Business/Cqrs/EftTransactionCqrs.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Vb.Base.Response;
+using Vb.Schema;
+
+namespace Vb.Business.Cqrs;
+
+public record GetAllEftTransactionQuery() : IRequest<ApiResponse<List<EftTransactionResponse>>>;
+
+public record GetEftTransactionByIdQuery(int Id) : IRequest<ApiResponse<EftTransactionResponse>>;
+
+public record GetEftTransactionByParameterQuery(string ReferenceNumber)
+    : IRequest<ApiResponse<List<EftTransactionResponse>>>;
+
+public record CreateEftTransactionCommand(EftTransactionRequest Model) : IRequest<ApiResponse<EftTransactionResponse>>;
+
+public record UpdateEftTransactionCommand(int Id, EftTransactionRequest Model) : IRequest<ApiResponse>;
+
+public record DeleteEftTransactionCommand(int Id) : IRequest<ApiResponse>;
