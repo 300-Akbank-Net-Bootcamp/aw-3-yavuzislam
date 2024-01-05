8b21aedeb9d713d1399a0f67abed104cc6e8ba14
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:09:54 +0300

    Create AccountTransactionsController.cs

---
+++ VbApi/Vb.Api/Controllers/AccountTransactionsController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Vb.Base.Response;
+using Vb.Business.Cqrs;
+using Vb.Schema;
+
+namespace VbApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountTransactionsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public AccountTransactionsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ApiResponse<List<AccountTransactionResponse>>> Get()
+    {
+        var operation = new GetAllAccountTransactionQuery();
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ApiResponse<AccountTransactionResponse>> Get(int id)
+    {
+        var operation = new GetAccountTransactionByIdQuery(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPost]
+    public async Task<ApiResponse<AccountTransactionResponse>> Post(
+        [FromBody] AccountTransactionRequest accountTransaction)
+    {
+        var operation = new CreateAccountTransactionCommand(accountTransaction);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] AccountTransactionRequest accountTransaction)
+    {
+        var operation = new UpdateAccountTransactionCommand(id, accountTransaction);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteAccountTransactionCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+}
