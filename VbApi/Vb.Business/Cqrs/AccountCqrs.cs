0612e1b6c25840785d64b1111ea8e56377f07a2f
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:10:47 +0300

    Create AccountCqrs.cs

---
+++ VbApi/Vb.Business/Cqrs/AccountCqrs.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Vb.Base.Response;
+using Vb.Schema;
+
+namespace Vb.Business.Cqrs;
+
+public record GetAllAccountQuery() : IRequest<ApiResponse<List<AccountResponse>>>;
+
+public record GetAccountByIdQuery(int Id) : IRequest<ApiResponse<AccountResponse>>;
+
+public record GetAccountByParameterQuery(int CustomerId, int AccountNumber)
+    : IRequest<ApiResponse<List<AccountResponse>>>;
+
+public record CreateAccountCommand(AccountRequest Model) : IRequest<ApiResponse<AccountResponse>>;
+
+public record UpdateAccountCommand(int Id, AccountRequest Model) : IRequest<ApiResponse>;
+
+public record DeleteAccountCommand(int Id) : IRequest<ApiResponse>;
