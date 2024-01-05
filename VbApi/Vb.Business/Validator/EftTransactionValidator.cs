70691cff7107d045b3878babcd781f7f12f84c46
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-05 23:11:25 +0300

    Create EftTransactionValidator.cs

---
+++ VbApi/Vb.Business/Validator/EftTransactionValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Vb.Schema;
+
+namespace Vb.Business.Validator;
+
+public class EftTransactionValidator : AbstractValidator<EftTransactionRequest>
+{
+    public EftTransactionValidator()
+    {
+        RuleFor(x => x.AccountId).NotEmpty().WithMessage("Account ID is required.");
+        RuleFor(x => x.TransactionDate).NotEmpty().WithMessage("Transaction Date is required.");
+        RuleFor(x => x.Amount).NotEmpty().WithMessage("Amount is required.")
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+        RuleFor(x => x.Description).MaximumLength(300).WithMessage("Description must not exceed 300 characters.");
+        RuleFor(x => x.ReferenceNumber).NotEmpty().WithMessage("Reference Number is required.")
+            .MaximumLength(50).WithMessage("Reference Number must not exceed 50 characters.");
+        RuleFor(x => x.SenderAccount).NotEmpty().WithMessage("Sender Account is required.")
+            .MaximumLength(50).WithMessage("Sender Account must not exceed 50 characters.");
+        RuleFor(x => x.SenderIban).NotEmpty().WithMessage("Sender IBAN is required.")
+            .MaximumLength(50).WithMessage("Sender IBAN must not exceed 50 characters.");
+        RuleFor(x => x.SenderName).NotEmpty().WithMessage("Sender Name is required.")
+            .MaximumLength(50).WithMessage("Sender Name must not exceed 50 characters.");
+    }
+}
